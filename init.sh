d3e58cc277bf06f9e29909e8d6fa9e15a87ff860
UTAGAWA Kiki <utagawakiki@gmail.com>
2015-10-18 03:04:08 +0900

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+if ! type git >/dev/null 2>&1; then
+  echo "Installing git."
+  sudo apt-get install git
+else
+  echo "git is already installed."
+fi
+
+if [ ! -d ~/dotfiles ]; then
+  echo "Setting dotfiles."
+  git clone git@github.com:utgw/dotfiles ~/dotfiles
+  cd ~/
+  for f in ~/dotfiles/.*; do
+    ln -f $f
+  done
+else
+  echo "dotfiles are already configured."
+fi
+
+
+if ! type pyenv >/dev/null 2>&1; then
+  echo "Installing pyenv."
+  git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+else
+  echo "pyenv is already installed."
+fi
+
+if ! type rbenv >/dev/null 2>&1; then
+  echo "Installing rbenv."
+  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+else
+  echo "rbenv is already installed."
+fi
+
+# It doesn't work...
+#if ! type nvm >/dev/null 2>&1; then
+if [ ! -d ~/.nvm ]; then
+  echo "Installing nvm."
+  git clone https://github.com/creationix/nvm.git ~/.nvm
+  source ~/.nvm/nvm.sh
+else
+  echo "nvm is already installed."
+fi
+
+source ~/.bash_profile
+echo "Finished."
