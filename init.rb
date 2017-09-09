e0779731cf3166fc2e1ee176a04b7aee7158bd9b
UTAGAWA Kiki <utagawakiki@gmail.com>
2017-09-09 10:40:52 +0900

    Migrate to itamae recipe

---
+++ init.rb
@@ -0,0 +1,37 @@
+define :download, url: nil, to: nil do
+  url = params[:url]
+  to = params[:to]
+
+  execute "Download #{url} to #{to}" do
+    command "curl '#{url}' > #{to}"
+    not_if File.file? to
+  end
+end
+
+# Copy dotfiles
+%w|
+.aliases
+.zshrc
+.zprofile
+.zlogout
+.gitconfig
+.gitignore.global
+.vimrc
+.tmux.conf
+|.each do |f|
+  dest = File.join ENV['HOME'], f
+  remote_file dest do
+    action :create
+    mode "0644"
+  end
+end
+
+download "git-prompt" do
+  url "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
+  to File.join Dir.pwd, "git-prompt.sh"
+end
+
+download "git-completion" do
+  url "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+  to File.join Dir.pwd, "git-completion.bash"
+end
